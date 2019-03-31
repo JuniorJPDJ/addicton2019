542984402068089d97036e9877438384d4127c10
Bartosz Krakowiak <bkrakowiak@man.poznan.pl>
2019-03-31 10:31:55 +0200

    options

--- options/script.js
+++ options/script.js
@@ -1,1 +1,18 @@
-document.getElementById('myHeading').style.color = 'red'
+(async function() {
+    browser.runtime.onMessage.addListener((message) => {
+        if (message.command === "") {
+            writeStats(message.num);
+        }
+    });
+
+    let req = browser.runtime.sendMessage({
+        message: 'timeAnalisis',
+    });
+    writeStats(await req);
+
+    function writeStats(stats){
+        // let div = document.getElementById('statistics');
+        // div.append();
+        console.log(stats);
+    }
+})();
